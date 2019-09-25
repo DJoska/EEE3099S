8537e2fb1f18bb64405520d3e89d9157d4362378
Daniel <danieljoska07@gmail.com>
2019-09-25 07:37:56 +0200

    Added interrupt and cleaned up

--- src/main.c
+++ src/main.c
@@ -1,11 +1,11 @@
 //********************************************************************
-//*                    EEE2046F C example                            *
+//*                    EEE3099S Maze Solver                          *
 //*==================================================================*
 //* WRITTEN BY:    	                 		                         *
 //* DATE CREATED:                                                    *
 //* MODIFIED:                                                        *
 //*==================================================================*
-//* PROGRAMMED IN: Eclipse Neon 3 Service Release 1 (4.4.1)          *
+//* PROGRAMMED IN: Atollic TrueSTUDIO 9.3                            *
 //* DEV. BOARD:    UCT STM32 Development Board                       *
 //* TARGET:	   STMicroelectronics STM32F051C6                        *
 //*==================================================================*
@@ -32,6 +32,9 @@ uint8_t mid_r = 0; //middle right sensor reading
 uint8_t c_l = 0; //corner left sensor reading
 uint8_t c_r = 0; //corner right sensor reading
 uint8_t ADC_reference=0; //if above this value then we know that sensor reading will be high and on black line (inverse is true)
+int sensor1;
+int sensor2;
+int sensor3;
 //====================================================================
 // FUNCTION DECLARATIONS
 //====================================================================
@@ -41,6 +44,9 @@ int sampleADC(int channel);
 void init_ADC(void);
 void init_PWM(void);
 int read_sensor(int channel);
+void init_EXTI(void);
+void EXTI2_3_IRQHandler (void);
+void get_moving(void);
 //====================================================================
 // MAIN FUNCTION
 //====================================================================
@@ -50,40 +56,11 @@ void main (void)
 	init_ADC();
 	init_PWM();
 	init_LCD();
+	init_EXTI();
 
+	GPIOB -> ODR = 0b0;
 	for(;;)
 	{
-		int sensor1 = read_sensor(1);
-		int sensor2 = read_sensor(2);
-		int sensor3 = read_sensor(3);
-
-		if(sensor2<5 && sensor1>5 && sensor3>5){
-			//move forward
-		}
-		if(sensor2<5 && sensor1>20){
-			//correct left
-		}
-		if(sensor2<5 && sensor3>20){
-			//correct right
-		}
-		if(sensor2>10 || sensor4<10 || sensor5<10){
-			//turn left
-		}
-
-		ADC1 -> CHSELR &= 0b0;	//disable channel
-
-		float led_duty = (float)(read_sensor(5) * (100/255.0));
-		TIM2->CCR3 = led_duty * 80; // Duty Cycle for PB10
-		TIM2->CCR4 = (100-led_duty)*80;
-
-		ADC1 -> CHSELR &= 0b0;	//disable channel
-
-		GPIOB->ODR = read_sensor(1);
-
-		ADC1 -> CHSELR &= 0b0;	//disable channel
-		delay(100);
-
-
 
 		//CHECK ADC REFERENCE TO EACH SENSOR CHANNEL AND MAKE A TURN OR GO STRAIGHT OR STOP AT JUNCTION etc based off input
 
@@ -93,31 +70,94 @@ void main (void)
 //====================================================================
 // FUNCTION DEFINITIONS
 //====================================================================
+void init_EXTI (void)
+{
+	RCC -> APB2ENR |= RCC_APB2ENR_SYSCFGCOMPEN; // ENABLE EXTI BUS CLK
+	SYSCFG -> EXTICR[0] |= SYSCFG_EXTICR1_EXTI3_PA; // MAP INTERRUPT TO PA3
+	EXTI -> IMR |= EXTI_IMR_MR3; // UNBLOCK INTERRUPT LINE 1 BUS
+	EXTI -> FTSR |= EXTI_FTSR_TR3; // CONDITION CHECK: FALLING-EDGE
+	NVIC_EnableIRQ(EXTI2_3_IRQn); // ENABLE LINE 0 &amp; LINE 1 INTERRUPT
+}
+
+void EXTI2_3_IRQHandler (void)
+{
+	EXTI -> PR |= EXTI_PR_PR3; // EXIT INTERRUPT
+
+	// User Interrupt Service Routine Here
+	get_moving();
+}
+
 void init_GPIO(void)
 {
 
 	RCC -> AHBENR |= RCC_AHBENR_GPIOAEN;	//enable port a
-	GPIOA -> MODER |= GPIO_MODER_MODER1; //set PA1, analogue mode
-	GPIOA -> MODER |= GPIO_MODER_MODER2; //set PA2, analogue mode
-	GPIOA -> MODER |= GPIO_MODER_MODER3; //set PA3, analogue mode
+	RCC -> AHBENR |= RCC_AHBENR_GPIOBEN;	//enable port b
+
+	GPIOA -> MODER &= ~GPIO_MODER_MODER1; //set PA1, input mode
+	GPIOA -> MODER &= ~GPIO_MODER_MODER2; //set PA2, input mode
+	GPIOA -> MODER &= ~GPIO_MODER_MODER3; //set PA3, input mode
+
+	//set pull-up resistors on ports A0-3
+	GPIOA->PUPDR |= (GPIO_PUPDR_PUPDR0_0|
+						GPIO_PUPDR_PUPDR1_0|
+						GPIO_PUPDR_PUPDR2_0|
+						GPIO_PUPDR_PUPDR3_0);
+
+	GPIOA -> MODER |= GPIO_MODER_MODER4; //set PA4, analogue mode
 	GPIOA -> MODER |= GPIO_MODER_MODER5; //set PA5, analogue mode
 	GPIOA -> MODER |= GPIO_MODER_MODER6; //set PA6, analogue mode
+
 	RCC  ->AHBENR |= RCC_AHBENR_GPIOBEN;
 	GPIOB->MODER  |= (GPIO_MODER_MODER0_0 |
 						GPIO_MODER_MODER1_0|
 						GPIO_MODER_MODER2_0|
 						GPIO_MODER_MODER3_0|
-						GPIO_MODER_MODER4_0|
-						GPIO_MODER_MODER5_0|
-						GPIO_MODER_MODER6_0|
-						GPIO_MODER_MODER7_0);	//setting LEDs to output mode
+						GPIO_MODER_MODER12_0|
+						GPIO_MODER_MODER13_0|
+						GPIO_MODER_MODER14_0|
+						GPIO_MODER_MODER15_0);	//setting LEDs to output mode
 	GPIOB->ODR = 0b0;
 }
+
 void Delay(void)
 {
 for(int i=0; i<1000; i++)
    for(int k=0; k<2000; k++);
 }
+
+void get_moving(void){
+	for(;;){
+		sensor1 = read_sensor(4);
+		sensor2 = read_sensor(5);
+		sensor3 = read_sensor(6);
+
+		GPIOB -> ODR = 0b1001000000000000;
+
+		//TODO Try 5 as the threshold
+		if(sensor1<=2){
+			//move forward
+			TIM2->CCR3 = 50*80;
+			TIM2->CCR4 = 50*80;
+			//GPIOB -> ODR = 0b1001000000000000;
+		}
+		else if(sensor2<=2){
+			//correct left
+			TIM2->CCR3 = 30*80;
+			TIM2->CCR4 = 50*80;
+			//GPIOB -> ODR = 0b1010000000000000;
+		}else if(sensor3<=2){
+			//correct right
+			TIM2->CCR3 = 50*80;
+			TIM2->CCR4 = 30*80;
+			//GPIOB -> ODR = 0b0110000000000000;
+		}else if(sensor1>2 && sensor2>2 && sensor3>2){
+			TIM2->CCR3 = 50*80;
+			TIM2->CCR4 = 50*80;
+			GPIOB -> ODR = 0b1010000000000000;
+		}
+	}//end for
+
+}//end get moving function
 
 void turn_left(void){
 	//set motors to turn left
@@ -170,21 +210,6 @@ void init_PWM(void) {
 	TIM2->CCER |= TIM_CCER_CC3E;//PB10
 	TIM2->CCER |= TIM_CCER_CC4E; //PB11
 	TIM2->CR1 |= TIM_CR1_CEN; //start TIM2
-
-	//RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-	//configure PB3 and PB10 to alternate function mode
-	//GPIOB->MODER |= GPIO_MODER_MODER12_1; // PB12 = AF LEFT INPUT 2
-	//GPIOB->MODER |= GPIO_MODER_MODER13_1; // PB13 = AF RIGHT INPUT 2
-	//GPIOB->AFR[1] |= (2 << (4*(12 - 8))); //map PB12 to TIM2 channel 3 using AF2 of AFRH
-	//GPIOB->AFR[0] |= (2 << (4*(13 - 8))) ; //map PB13 to TIM2 channel 2 using AF2 of AFRL
-	//TIM2->ARR = 8000; // set ARR for approximately 1kHz PWM frequency
-	// set PWM mode 1 for both channels
-	//TIM2->CCMR2 |= (TIM_CCMR2_OC3M_2 | TIM_CCMR2_OC3M_1); // PWM Mode 1 for PB12
-	//TIM2->CCMR2 |= (TIM_CCMR2_OC4M_2 | TIM_CCMR2_OC4M_1); // PWM Mode 1 for PB13
-	// enable output compare on channels 2 and 3 of TIM2
-	//TIM2->CCER |= TIM_CCER_CC3E;//PB12
-	//TIM2->CCER |= TIM_CCER_CC4E; //PB13
-	//TIM2->CR1 |= TIM_CR1_CEN; //start TIM2
 	}
 
 	//Takes in ADC channel to be converted (1,2,3,4,5,6). Corresponding channel is selected
@@ -194,25 +219,25 @@ int read_sensor(int channel){
 
 	int channel_bitmask;
 	switch(channel){
-	case 1:
-	channel_bitmask = ADC_CHSELR_CHSEL1; // Channel 1 (mapped to f sensor)
+		case 1:
+			channel_bitmask = ADC_CHSELR_CHSEL1; // Channel 1 (mapped to f sensor)
 
-	break;
-	case 2:
-	channel_bitmask = ADC_CHSELR_CHSEL2; // Channel 2 (mapped to mid_r sensor)
+			break;
+		case 2:
+			channel_bitmask = ADC_CHSELR_CHSEL2; // Channel 2 (mapped to mid_r sensor)
 
-	break;
-	case 3:
-	channel_bitmask = ADC_CHSELR_CHSEL3; // Channel 3 (mapped to mid_l sensor)
+			break;
+		case 3:
+			channel_bitmask = ADC_CHSELR_CHSEL3; // Channel 3 (mapped to mid_l sensor)
 
-	break;
-	case 5:
-	channel_bitmask = ADC_CHSELR_CHSEL5; // Channel 5 (mapped to c_r sensor)
+			break;
+		case 5:
+			channel_bitmask = ADC_CHSELR_CHSEL5; // Channel 5 (mapped to c_r sensor)
 
-	break;
-	case 6:
-	channel_bitmask = ADC_CHSELR_CHSEL6; // Channel 6 (mapped to c_l sensor)
-	break;
+			break;
+		case 6:
+			channel_bitmask = ADC_CHSELR_CHSEL6; // Channel 6 (mapped to c_l sensor)
+			break;
 
 }
 	int val = sampleADC(channel_bitmask);
